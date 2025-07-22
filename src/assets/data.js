6faff3ade511cbd3def5339581925c73d4f04f47
Maher Aounallah <maheraounallah7777777@gmail.com>
2025-07-22 15:53:54 +0100

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,85 @@
+
+const data = [
+  {
+    question: "What is the real name of Iron Man?",
+    option1: "Steve Rogers",
+    option2: "Tony Stark",
+    option3: "Bruce Banner",
+    option4: "Stephen Strange",
+    answer: 2
+  },
+  {
+    question: "Which Infinity Stone does Vision have on his forehead?",
+    option1: "Time Stone",
+    option2: "Power Stone",
+    option3: "Mind Stone",
+    option4: "Soul Stone",
+    answer: 3
+  },
+  {
+    question: "Who is the sister of T’Challa in Black Panther?",
+    option1: "Okoye",
+    option2: "Shuri",
+    option3: "Nakia",
+    option4: "Ramonda",
+    answer: 2
+  },
+  {
+    question: "What is Captain America's shield made of?",
+    option1: "Adamantium",
+    option2: "Titanium",
+    option3: "Uru",
+    option4: "Vibranium",
+    answer: 4
+  },
+  {
+    question: "What is the name of Thor’s hammer?",
+    option1: "Stormbreaker",
+    option2: "Gungnir",
+    option3: "Mjolnir",
+    option4: "Necrosword",
+    answer: 3
+  },
+  {
+    question: "Who is the main villain in Avengers: Infinity War?",
+    option1: "Ultron",
+    option2: "Loki",
+    option3: "Thanos",
+    option4: "Red Skull",
+    answer: 3
+  },
+  {
+    question: "Which character turns into the Hulk?",
+    option1: "Bruce Banner",
+    option2: "Peter Parker",
+    option3: "Tony Stark",
+    option4: "Stephen Strange",
+    answer: 1
+  },
+  {
+    question: "What metal is injected into Wolverine’s skeleton?",
+    option1: "Vibranium",
+    option2: "Steel",
+    option3: "Adamantium",
+    option4: "Carbonadium",
+    answer: 3
+  },
+  {
+    question: "What is the name of Peter Quill’s alter ego?",
+    option1: "Nova",
+    option2: "Star-Lord",
+    option3: "Rocket",
+    option4: "Drax",
+    answer: 2
+  },
+  {
+    question: "Who sacrificed herself for the Soul Stone in Endgame?",
+    option1: "Gamora",
+    option2: "Scarlet Witch",
+    option3: "Nebula",
+    option4: "Black Widow",
+    answer: 4
+  }
+];
+
+export default data;
