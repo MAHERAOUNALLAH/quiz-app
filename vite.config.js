802ef5967cd9ab347381bbeb6e986aeaeb6e476f
MAHERAOUNALLAH <Aounallah.maher@yahoo.com>
2025-07-23 13:41:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '', 
+  base: '/quiz-app/',  
   plugins: [react()],
 });
